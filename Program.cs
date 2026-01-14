a13722c6dca0d0dc4383a71700863020343f613f
jeeftaibrahim <jeeftaif@gmail.com>
2026-01-14 09:52:30 +0700

    latian method 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahitem;
+
+decimal hargaitem, diskon, totalbayar;
+
+static decimal hitungdiskon(int jmlitem)
+{
+    if (jmlitem == 5) return 50000m;
+    else if (jmlitem == 10) return 75000m;
+    else return 0;
+}
+
+static decimal hitungtotal(int jmlitem, decimal hrgaitem, decimal diskonitem)
+{
+    return (jmlitem * hrgaitem - diskonitem);
+}
+
+Console.Write("Masukan Jumlah item:");
+jumlahitem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukan harga item: ");
+hargaitem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungdiskon(jumlahitem);
+
+totalbayar = hitungtotal(jumlahitem, hargaitem, diskon);
+
+Console.WriteLine($"total yang harus dibayar adalah Rp {totalbayar}");
